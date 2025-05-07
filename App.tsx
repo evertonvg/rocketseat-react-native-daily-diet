23a8f456a8d3af0e0ee371ab7bc6e8b9ba116100
Everton <evertoniee@yahoo.com.br>
2025-05-07 10:26:22 -0300

    fix: add styled components crash resolution in react-native 0.79

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+export default function App() {
+  return (
+    <Container>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </Container>
+  );
+}
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
